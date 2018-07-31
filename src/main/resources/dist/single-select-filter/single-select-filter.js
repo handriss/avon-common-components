faeb4041a6ef0d556131284e6ad62806252c2195
handriss <andras.hinkel@blackbelt.hu>
2018-07-31 18:08:46 +0200

    Try simple example and compile with tsc

---
+++ src/main/resources/dist/single-select-filter/single-select-filter.js
@@ -0,0 +1,7 @@
+var SelectableOption = /** @class */ (function () {
+    function SelectableOption(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return SelectableOption;
+}());

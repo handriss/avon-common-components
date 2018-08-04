6796d2fed597b699e7368a88d84874c861f43d0c
handris <andrashinkel@gmail.com>
2018-08-04 11:02:48 +0200

    Implement simple unit tests using jasmine

---
+++ src/main/resources/src/single-select-filter/single-select-filter.spec.ts
@@ -0,0 +1,9 @@
+describe('Single Select Filter', function () {
+    it('should run a simple example test', function () {
+        expect(true).toBe(true);
+    });
+
+    it('should not fail', function () {
+        expect(false).toBe(false);
+    });
+});
